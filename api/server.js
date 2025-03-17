dc3af6e568f9b6f6c0c5fee19dc602fe4a55c714
mrehab <rehab123335@gmail.com>
2025-03-17 12:39:26 +0500

    'deployed but fixing the serverless issue'

---
+++ api/server.js
@@ -0,0 +1,15 @@
+const jsonServer = require("json-server");
+
+const server = jsonServer.create();
+const middlewares = jsonServer.defaults();
+
+const data = jsonServer.router("data.json");
+const tvshows = jsonServer.router("tvshows.json");
+
+server.use(middlewares);
+server.use("/moviesdata", data);
+server.use("/tv", tvshows);
+
+module.exports = (req, res) => {
+  server(req, res); // No listen here, Vercel handles it
+};
