6807d638281ab137a7e768c8006ccd7844266e6e
mrehab <rehab123335@gmail.com>
2025-03-31 02:39:55 +0500

    api folder trying issue again finally

--- api/server.js
+++ api/server.js
@@ -1,15 +1,15 @@
+// See https://github.com/typicode/json-server#module
 const jsonServer = require("json-server");
-
 const server = jsonServer.create();
+const router = jsonServer.router("data.json");
 const middlewares = jsonServer.defaults();
 
-const data = jsonServer.router("data.json");
-const tvshows = jsonServer.router("tvshows.json");
-
 server.use(middlewares);
-server.use(data);
-// server.use("/tv", tvshows);
+// Add this before server.use(router)
+server.use(router);
+server.listen(3001, () => {
+  console.log("JSON Server is running");
+});
 
-module.exports = (req, res) => {
-  server(req, res); // No listen here, Vercel handles it
-};
+// Export the Server API
+module.exports = server;
